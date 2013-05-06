0d56cb36b53aaeab45f3bc0dcdc435d734db499e
StephenWang <Dr.StephenWang@gmail.com>
2013-05-06 14:38:54 +0800

    TF-IDF Result Complete

--- TF-IDF/src/cs/dcd/tfidf/Main.java
+++ TF-IDF/src/cs/dcd/tfidf/Main.java
@@ -1,6 +1,8 @@
 package cs.dcd.tfidf;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,43 +10,60 @@ import cs.dcd.tfidf.ReadFiles;
 
 public class Main {
 	
+	public static final double TDIDF_MIN_VALUE = 0.003;
+	
     public static void main(String[] args) throws IOException
     {
     	String inputPath = ReadFiles.readDirectoryPath("Input Target Path: ");
+    	String outputPath = ReadFiles.readDirectoryPath("Output Target Path: ");
     	Map<Integer, String> fileMap = ReadFiles.readFileMap(inputPath);
     	
     	Map<String, HashMap<String, Integer>> normal = TFIDF.NormalTFOfAll(fileMap);
-        for (String filename : normal.keySet())
+    	PrintWriter fileTFOutput = new PrintWriter(new FileWriter(outputPath + "\\TF.txt"));
+    	for (String filename : normal.keySet())
         {
-            System.out.println("fileName " + filename);
-            System.out.println("TF " + normal.get(filename).toString());
-        }    	
-    	
-        System.out.println("-----------------------------------------");
-        
-    	Map<String, HashMap<String, Double>> notNarmal = TFIDF.tfOfAll(fileMap);
-    	for (String filename : notNarmal.keySet())
-    	{
-            System.out.println("fileName " + filename);
-            System.out.println("TF " + notNarmal.get(filename).toString());
+    		fileTFOutput.println("fileName " + filename);
+    		fileTFOutput.println("TF " + normal.get(filename).toString());
         }
-
-        System.out.println("-----------------------------------------");
+    	fileTFOutput.close();
+        
+    	Map<String, HashMap<String, Double>> notNormal = TFIDF.tfOfAll(fileMap);
+    	PrintWriter fileTFNormalOutput = new PrintWriter(new FileWriter(outputPath + "\\TFNormal.txt"));
+    	for (String filename : notNormal.keySet())
+    	{
+    		fileTFNormalOutput.println("fileName " + filename);
+    		fileTFNormalOutput.println("TF " + notNormal.get(filename).toString());
+        }
+    	fileTFNormalOutput.close();
   	
     	Map<String, Double> idf = TFIDF.idf(fileMap);
+    	PrintWriter fileIDFOutput = new PrintWriter(new FileWriter(outputPath + "\\IDF.txt"));
     	for (String word : idf.keySet())
     	{
-            System.out.println("keyword :" + word + " idf: " + idf.get(word));
+    		fileIDFOutput.println("keyword :" + word + " idf: " + idf.get(word));
         }
-
-        System.out.println("-----------------------------------------");
+    	fileIDFOutput.close();
 
     	Map<String, HashMap<String, Double>> tfidf = TFIDF.tfidf(fileMap);
+    	PrintWriter TFIDFOutput = new PrintWriter(new FileWriter(outputPath + "\\TFIDF.txt"));
     	for (String filename : tfidf.keySet())
     	{
-            System.out.println("fileName " + filename);
-            System.out.println(tfidf.get(filename));
+    		TFIDFOutput.println("fileName " + filename);
+    		TFIDFOutput.println("TFIDF " + tfidf.get(filename).toString());
+    		
+    		PrintWriter fileTFIDFOutput = new PrintWriter(new FileWriter(outputPath + "\\" + filename));
+    		for (String itemString : tfidf.get(filename).keySet())
+    		{
+    			if (tfidf.get(filename).get(itemString) > TDIDF_MIN_VALUE)
+    			{
+    				fileTFIDFOutput.println(itemString);
+    			}
+    		}
+    		fileTFIDFOutput.close();
         }
+    	TFIDFOutput.close();
+    	
+    	System.out.println("Done");
     	
     }
 
